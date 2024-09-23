17e2bce8cf0ba3b6ef90ce9a420c8901ece4bbd1
zmc0806 <129798536+zmc0806@users.noreply.github.com>
2024-09-24 07:02:36 +0800

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
+from prophet import Prophet
 
 
 st.title('stock data')
@@ -13,8 +14,27 @@ end_date = st.date_input("choose end data", pd.to_datetime('today'))
 if st.button('get data'):
     ticker_data = yf.Ticker(ticker_symbol)
     ticker_df = ticker_data.history(period='1d', start=start_date, end=end_date)
-    st.write(f"{ticker_symbol} stock data")
-    st.write(ticker_df)
+  
+    if ticker_df.empty:
+        st.error(f"can not {ticker_symbol} stock data please check")
+    else:
+        st.write(f"{ticker_symbol} stock data")
+        st.write(ticker_df)
 
+        st.line_chart(ticker_df['Close'])
 
+        st.write(f"{ticker_symbol} future prediction")
 
+        df_prophet = ticker_df.reset_index()[['Date', 'Close']]
+        df_prophet.columns = ['ds', 'y']  
+        df_prophet['ds'] = pd.to_datetime(df_prophet['ds']).dt.tz_localize(None)  
+
+        model = Prophet()
+        model.fit(df_prophet)
+
+        future = model.make_future_dataframe(periods=365)
+        forecast = model.predict(future)
+
+        st.write(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']])
+else:
+    st.info("Please enter the ticker symbol and click the Get Data button")
